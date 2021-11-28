864440f2bfbaa80092a4b48fcf1ec06b9534e5aa
RyuYamamoto <rottertarmenation.y11.r18@gmail.com>
2021-11-28 15:57:59 +0900

    add utils.hpp

--- controller/MotionEngine.cpp
+++ controller/MotionEngine.cpp
@@ -12,12 +12,13 @@ void MotionEngine::initialize(cnoid::SimpleControllerIO* io)
     joint->setActuationMode(cnoid::Link::JointTorque);
     io->enableIO(joint);
 
-    JointAngle joint_angle_;
-    joint_angle_.id = joint->jointId();
-    joint_angle_.name = joint->jointName();
-    joint_angle_.ref_q = joint->q();
-    joint_angle_.prev_q = joint_angle_.ref_q;
-    joint_angles_.emplace_back(joint_angle_);
+    JointAngle joint_angle;
+    joint_angle.id = joint->jointId();
+    joint_angle.name = joint->jointName();
+    joint_angle.ref_q = joint->q();
+    joint_angle.prev_ref_q = joint_angle.ref_q;
+    joint_angle.prev_q = joint_angle.ref_q;
+    joint_angles_.emplace_back(joint_angle);
   }
 
   kinematics_ = std::make_shared<Kinematics>(body_);
@@ -36,7 +37,8 @@ void MotionEngine::jointControl()
     cnoid::Link* joint = body_->joint(idx);
     double q = joint->q();
     double dq = (q - joint_angles_[idx].prev_q) / dt_;
-    double u = (joint_angles_[idx].ref_q - q) * p_gain[idx] + (0.0 - dq) * d_gain[idx];
+    double ref_dq = (joint_angles_[idx].ref_q - joint_angles_[idx].prev_ref_q) / dt_;
+    double u = (joint_angles_[idx].ref_q - q) * p_gain[idx] + (ref_dq - dq) * d_gain[idx];
     joint->u() = std::max(std::min(u, 500.0), -500.0);
     joint_angles_[idx].prev_q = q;
   }
