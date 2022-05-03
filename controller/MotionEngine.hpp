509757222fd1bef2bdd303347b1c7c02d5c1bde5
RyuYamamoto <rottertarmenation.y11.r18@gmail.com>
2022-05-03 10:59:04 +0900

    add inverse kinematics demo

--- controller/MotionEngine.hpp
+++ controller/MotionEngine.hpp
@@ -1,27 +1,40 @@
 #ifndef _MOTION_ENGINE_HPP_
 #define _MOTION_ENGINE_HPP_
 
-#include "utils.hpp"
 #include "Kinematics.hpp"
+#include "utils.hpp"
+
+#include <cnoid/Joystick>
+
+#include <iostream>
 
 class MotionEngine
 {
 public:
   MotionEngine();
 
-  void initialize(cnoid::SimpleControllerIO* io);
+  void initialize(cnoid::SimpleControllerIO * io);
 
   void control();
 
   void jointControl();
 
+  void receiveCommand(double * l_stick, double * r_stick);
+
 private:
   std::shared_ptr<Kinematics> kinematics_;
 
-  cnoid::Body* body_;
+  cnoid::Joystick joystick_;
+
+  cnoid::Body * body_;
 
   double dt_;
-  std::vector<JointAngle> joint_angles_;
+  std::map<std::string, JointAngle> joint_angles_;
+
+  cnoid::Vector3 right_foot_pos_;
+  cnoid::Vector3 right_foot_rot_;
+  cnoid::Vector3 left_foot_pos_;
+  cnoid::Vector3 left_foot_rot_;
 };
 
 #endif
